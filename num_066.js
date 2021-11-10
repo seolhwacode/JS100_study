ecc98df238cd3eef7c2aa164f4426a139eda6a4b
DESKTOP-T8KA0UH\rkdfb <seolhwacode@gmail.com>
2021-11-10 22:19:27 +0900

    2021.11.10 66~67

--- num_066.js
+++ num_066.js
@@ -25,3 +25,52 @@
 ["가능", "불가능", "가능", "가능", "가능"]
 ```
 */
+
+const tower = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"];
+const rule = "ABD";
+
+//정규식 : rule 에 해당하지 않는 모든 값
+//g : 정규식에 해당하는 모든 것 찾기
+const reg = new RegExp(`[^${rule}]`, 'g');
+
+let towerCopy = tower.map(function(value){    
+    //rule 에 해당하지 않는 글 전부 삭제
+    let tmp = value.replace(reg, '');
+    return tmp;
+});
+
+//결과 배열
+let result = [];
+
+//rule 을 따르는지 아닌지 체크하는 함수
+function isFollowingRule(value, rule){
+    //배열의 길이가 1 이하 -> 무조건 규칙 따름
+    if(value.length <= 1){
+        return true;
+    }
+
+    for(let i = 0; i < value.length - 1; i++){
+        //해당 i 번째 문자가 rule 에 위치한 index 를 리턴
+        //앞의 문자의 index가 뒤의 문자의 index 보다 크면 -> 규칙에 어긋남
+        if(rule.indexOf(value[i]) >  rule.indexOf(value[i + 1])){
+            //불가능
+            return false;
+        }
+    }
+    //끝까지 전부 돌고 나왔다 -> 규칙 따름
+    return true;
+}
+
+//규칙 따르는지 input 에 확인
+towerCopy.forEach((value) => {
+    if(isFollowingRule(value, rule)){
+        //규칙 따름
+        result.push('가능');
+    }else{
+        //규칙 따르지 않음
+        result.push('불가능');
+    }
+});
+
+console.log(result);
+
